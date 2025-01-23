51b64ab85372a702f25a05d0928e2fdfe34c5d80
neilg2437 <146862583+neilg2437@users.noreply.github.com>
2025-01-23 14:26:55 -0500

    Update Q2:3_Query.sql

--- Q2:3_Query.sql
+++ Q2:3_Query.sql
@@ -1,10 +1,12 @@
-
+-- Select rewardsReceiptStatus, average totalSpent, and total purchasedItemCount
 SELECT 
-    rewardsReceiptStatus,
-    AVG(totalSpent) AS avg_spent,
-    SUM(purchasedItemCount) AS total_items
+    rewardsReceiptStatus, 
+    AVG(totalSpent) AS avg_spent, 
+    SUM(purchasedItemCount) AS total_items 
 FROM receipts
+-- Filter for receipts with a status of 'FINISHED' or 'REJECTED'
 WHERE rewardsReceiptStatus IN ('FINISHED', 'REJECTED')
+-- Group the results by receipt status 
 GROUP BY rewardsReceiptStatus;
 
 
